62d7155bc8715be4ce0f94165f5ef7868cd6148d
Regina Serakalala <regina.serakalala@multiply.co.za>
2024-07-05 13:41:42 +0200

    included test cases phone player class

--- src/test/java/com/teenthofabud/sports/football/PlayerTest.java
+++ src/test/java/com/teenthofabud/sports/football/PlayerTest.java
@@ -21,5 +21,25 @@ class PlayerTest {
     void testGetId(){
         assertEquals(player.getId(), 1L);
     }
+    @Test
+    void testEditFirstName(){
+        Player editedName = Player.editFirstName(1L, "John");
+        assertEquals("John", editedName.getFirstName());
+    }
+    @Test
+    void testEditLastName(){
+        Player editedName = Player.editLastName(1L, "Doe");
+        assertEquals("Doe", editedName.getLastName());
+    }
+    @Test
+    void testEditEmail(){
+        Player editedEmail = Player.editEmail(1L, "john@doe.com");
+        assertEquals("john@doe.com", editedEmail.getEmail());
+    }
+    @Test
+    void testEditPhone(){
+        Player editedPhone = Player.editPhoneNumber(1L, "0404059509504");
+        assertEquals("0404059509504", editedPhone.getPhoneNumber());
+    }
 
 }
