e919d1ffdfa7f6e663b061245c22348156a63ed9
Regina Serakalala <regina.serakalala@multiply.co.za>
2024-07-14 16:36:04 +0200

    removed slf4j and junit 4 dependencies in pom

--- src/test/java/com/teenthofabud/sports/football/AssociationTest.java
+++ src/test/java/com/teenthofabud/sports/football/AssociationTest.java
@@ -1,10 +1,21 @@
 package com.teenthofabud.sports.football;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AssociationTest {
     private Association association;
+    @BeforeEach
+    void setUp() {
+        association = Association.builder()
+                .name("Test")
+                .email("test@gmail.com")
+                .website("test.com")
+                .build();
+
+    }
     @Test
     void testAddAssociation(){
         Association addedAssociation = Association.add(association);
@@ -13,31 +24,32 @@ class AssociationTest {
     }
     @Test
     void testRemoveAssociation(){
-        Association removedAssociation = Association.remove(1L);
+        Association addAssociation = Association.add(association);
+        Association removedAssociation = Association.remove(addAssociation.getId());
         assertEquals(association, removedAssociation);
     }
     @Test
     void testGetAssociation(){
         Association.add(association);
-        Association retrieveAssociation = Association.get(1L);
+        Association retrieveAssociation = Association.get(association.getId());
         assertEquals(association, retrieveAssociation);
     }
     @Test
     void testEditName(){
-        Association.add(association);
-        Association editedName = Association.editName(1L, "Test");
+        Association addedAssociation = Association.add(association);
+        Association editedName = Association.editName(addedAssociation.getId(), association.getName());
         assertEquals("Test", editedName.getName());
     }
     @Test
     void testEditEmail(){
-        Association.add(association);
-        Association editedEmail = Association.editEmail(1L, "test@test.com");
+        Association addedAssociation = Association.add(association);
+        Association editedEmail = Association.editEmail(addedAssociation.getId(), addedAssociation.getEmail());
         assertEquals("test@test.com", editedEmail.getEmail());
     }
     @Test
     void testEditWebsite(){
-        Association.add(association);
-        Association editedWebsite = Association.editWebsite(1L, "testwebsite.com");
+        Association addedAssociation = Association.add(association);
+        Association editedWebsite = Association.editWebsite(addedAssociation.getId(), addedAssociation.getWebsite());
         assertEquals("testwebsite.com", editedWebsite.getWebsite());
     }
     @Test
