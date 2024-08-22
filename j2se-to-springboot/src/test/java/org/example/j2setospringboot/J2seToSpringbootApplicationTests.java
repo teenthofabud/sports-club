cc9745bb32db3e3c6c250786eefb31da325a2ed8
ginax <ginaserakalala@gmail.com>
2024-08-22 20:19:14 +0200

    included logging ,JPA,server configuration

---
+++ j2se-to-springboot/src/test/java/org/example/j2setospringboot/J2seToSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.j2setospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2seToSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
