a741390e838f1186d5aad03608c033e980dd5e33
ginax <ginaserakalala@gmail.com>
2024-08-26 20:20:04 +0200

    created springboot package within j2se project

---
+++ src/main/java/com/teenthofabud/sports/football/j2setospringboot/SpringBootMain.java
@@ -0,0 +1,7 @@
+package com.teenthofabud.sports.football.j2setospringboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMain {
+}
